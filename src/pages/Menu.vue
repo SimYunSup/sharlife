e93b9398f386b7da6a5a8fb5d6867e9f648a73fe
Pickhealer <pedogunu@gmail.com>
2019-12-09 16:31:54 +0900

    Add Menu Page

---
+++ src/pages/Menu.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+      <div class="absolute-full flex flex-center">
+        <div class="text-h6 text-centerflex flex-center">
+          없습니다.
+        </div>
+      </div>
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  name: 'parkingInfo'
+}
+</script>
+
+<style scoped>
+
+</style>
